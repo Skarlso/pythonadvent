f9646dd8722b909c4abb1fde89cfa8b26c91374b
Gergely Brautigam <skarlso777@gmail.com>
2016-05-04 18:16:45 +0200

    Part2.

---
+++ day1/part2.py
@@ -0,0 +1,19 @@
+from functools import reduce
+
+with open('input.txt', 'r') as f:
+    data=f.read().replace('\n', '')
+
+location = 1
+index = 0
+
+for i, ch in enumerate(data):
+    if ch == '(':
+        location += 1
+    else:
+        location -= 1
+
+    if location < 0:
+        index = i
+        break
+
+print(index)
