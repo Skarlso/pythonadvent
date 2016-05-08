478febf6794299f0504947fb7362577a27f75e29
Gergely Brautigam <skarlso777@gmail.com>
2016-05-08 08:57:55 +0200

    Part 1 finished. Needed the +1 on the range.

--- day7/part1.py
+++ day7/part1.py
@@ -6,20 +6,19 @@ with open('input.txt') as f:
     for line in f:
         line.rstrip()
         split = line.split()
+        fromx, fromy, tox, toy = 0, 0, 0, 0
         if split[0] == "turn":
             fromx, fromy = map(int, split[2].split(","))
             tox, toy = map(int, split[4].split(","))
-            for i in range(fromx, tox):
-                for j in range(fromy, toy):
-                    if split[1] == "on":
-                        grid[i][j] = True
-                    elif split[1] == "off":
-                        grid[i][j] = False
         elif split[0] == "toggle":
             fromx, fromy = map(int, split[1].split(","))
             tox, toy = map(int, split[3].split(","))
-            for i in range(fromx, tox):
-                for j in range(fromy, toy):
+
+        for i in range(fromx, tox + 1):
+            for j in range(fromy, toy + 1):
+                if split[0] == "turn":
+                    grid[i][j] = True if split[1] == "on" else False
+                elif split[0] == "toggle":
                     grid[i][j] = not grid[i][j]
 
 turnedOn = 0
